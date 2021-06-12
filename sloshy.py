5ebdc27a6d8b05870343eecce3156ab116e02997
tripleee <tripleee@users.noreply.github.com>
2021-06-12 19:29:30 +0300

    sloshy.py: major OOP refactoring
    Rooms are a separate class; enable local-only run with local=True
    
    Supporting act: LocalClient mock object for chatless operation; separate Chatclients object to keep track of chat clients for each server

--- sloshy.py
+++ sloshy.py
@@ -14,20 +14,127 @@ import chatexchange.client
 from scrape_chat import Transcript
 
 
+class LocalClient:
+    """
+    Simple mock ChatExchange.client with no actual networking functionality
+    """
+    def __init__(self, host='stackexchange.com', email=None, password=None):
+        self.host = host
+        self.email = email
+        self.password = password
+
+        self.room = None
+
+    def login(self, email=None, password=None):
+        logging.info('local - not logging in to %s', self.host)
+
+    def get_room(self, id: int):
+        logging.info('local - not fetching %s/rooms/%s', self.host, id)
+        self.room = id
+        return self
+
+    def join(self):
+        assert self.room is not None
+        logging.info('local - not joining %s/rooms/%i', self.host, self.room)
+
+    def send_message(self, message: str):
+        assert self.room is not None
+        logging.info(
+            'local - %s/rooms/%i: not sending message', self.host, self.room)
+        print(message)
+
+
+class Chatclients:
+    """
+    Singleton to keep track of chat servers, with one client for each.
+    """
+    def __init__(self, local=False):
+        """
+        Create a dispatcher for the servers we know about.
+        With local=True, create a LocalClient instead of a real
+        chatexchange one.
+        """
+        self.servers = dict()
+        self.email = None
+        self.password = None
+        self.local = local
+
+    def authnz(self, email: str, password: str):
+        self.email = email
+        self.password = password
+
+    def get_client_for_server(self, server: str) -> chatexchange.client.Client:
+        assert self.email is not None
+        assert self.password is not None
+
+        if server not in self.servers:
+            assert server.startswith('chat.')
+            site = server[5:]
+            if self.local:
+                client = LocalClient(site)
+            else:
+                client = chatexchange.client.Client(site)
+            client.login(self.email, self.password)
+            self.servers[server] = client
+        return self.servers[server]
+
+
+class Room:
+    """
+    Chat room with server, id, and a name for human-readable messages.
+    Also, handle to a Chatclients object which instantiates clients
+    as necessary.
+    """
+    def __init__(self, server: str, id: int, name: str, clients: Chatclients):
+        self.server = server
+        self.id = id
+        self.name = name
+        self.clients = clients
+
+        self.homeroom = False
+
+        self._chatroom = None
+
+    def set_as_home_room(self):
+        self.homeroom = True
+
+    def is_home_room(self) -> bool:
+        return self.homeroom
+
+    def send_message(self, message: str) -> None:
+        if self._chatroom is None:
+            chat = self.clients.get_client_for_server(self.server)
+            room = chat.get_room(self.id)
+            room.join()
+            self._chatroom = room
+        else:
+            room = self._chatroom
+        room.send_message(message)
+
+
 class Sloshy:
-    def __init__(self, conffile=None):
+    def __init__(self, conffile=None, local=False):
+        """
+        Create a Sloshy instance by parsing the supplied YAML file.
+
+        With local=True, don't connect to chat rooms.
+        """
         self.conffile = conffile
-        self.rooms = dict()
-        self.chatclient = dict()
+        self.local = local
+
+        self.rooms = []
+        self.homeroom = None
+        self.chatclients = None
         self.email = ''
         self.password = ''
-        self.homeroom = None
+
         if conffile:
             self.load_conf(conffile)
         if 'SLOSHY_EMAIL' in environ:
             self.email = environ.get('SLOSHY_EMAIL')
         if 'SLOSHY_PASSWORD' in environ:
             self.password = environ.get('SLOSHY_PASSWORD')
+        self.chatclients.authnz(self.email, self.password)
 
     def load_conf(self, conffile=None):
         """
@@ -41,17 +148,26 @@ class Sloshy:
             config = yaml.safe_load(filehandle)
         assert 'rooms' in config
 
-        self.rooms = config['rooms']
+        self.config = config['rooms']
+        self.rooms = []
+        self.homeroom = None
 
-        homeroom = None
-        for server, room in self.traverse_rooms():
-            assert 'id' in room
-            assert 'name' in room
-            if 'role' in room and room['role'] == 'home':
-                assert homeroom is None
-                homeroom = (server, room['id'])
-        assert homeroom is not None
-        self.homeroom = homeroom
+        clients = Chatclients(local=self.local)
+        self.chatclients = clients
+
+        for item in self.config:
+            for server, rooms in item.items():
+                for idx in range(len(item[server])):
+                    room = item[server][idx]
+                    assert 'id' in room
+                    assert 'name' in room
+                    roomobj = Room(server, room['id'], room['name'], clients)
+                    self.rooms.append(roomobj)
+                    if 'role' in room and room['role'] == 'home':
+                        assert self.homeroom is None
+                        self.homeroom = roomobj
+                        roomobj.set_as_home_room()
+        assert self.homeroom is not None
 
         if 'auth' in config:
             self.email = config['auth']['email']
@@ -70,41 +186,20 @@ class Sloshy:
             '♫ the heat never bothered me anyway🎶',
             'Kilr^WSloshy the Thawman was here!'))
 
-    def traverse_rooms(self):
+    def send_chat_message(self, room: Room, message: str):
         """
-        Loop over room structure; yield one (server, room) tuple at a time
+        Send chat message.  If not self.local, sleep after sending.
         """
-        for item in self.rooms:
-            for server, rooms in item.items():
-                for idx in range(len(item[server])):
-                    room = item[server][idx]
-                    yield server, room
+        assert self.chatclients is not None
+        room.send_message(message)
+        if not self.local:
+            sleep(3)
 
-    def chat(self, server, room):
+    def notice(self, room: Room):
         """
-        Establish a chat client connected to room on server
+        Drop a thawing notice in room
         """
-        assert isinstance(room, int)
-        if server not in self.chatclient:
-            assert server.startswith('chat.')
-            site = server[5:]
-            client = chatexchange.client.Client(site)
-            client.login(self.email, self.password)
-            self.chatclient[server] = client
-        else:
-            client = self.chatclient[server]
-
-        room = client.get_room(room)
-        room.join()
-        return room
-
-    def notice(self, server, room):
-        """
-        Drop a thawing notice in room on server
-        """
-        chat = self.chat(server, room)
-        chat.send_message(self.generate_chat_message())
-        sleep(3)
+        self.send_chat_message(room, self.generate_chat_message())
 
     def scan_rooms(self):
         """
@@ -112,29 +207,35 @@ class Sloshy:
         check if they are in danger of being frozen; if so, join
         and post a message.
         """
-        chat = self.chat(*self.homeroom)
-
         now = datetime.now()
         # Freeze schedule is 14 days; thaw a little before that,
         # just to be on the safe side.
         maxage = timedelta(days=12)
 
         fetcher = Transcript()
-        for server, room in self.traverse_rooms():
-            if 'role' in room and room['role'] == 'home':
+        homeroom = self.homeroom
+        for room in self.rooms:
+            if room.is_home_room():
                 continue
-            room_latest = fetcher.latest(room['id'], server)
+            room_latest = fetcher.latest(room.id, room.server)
             when = room_latest['when']
             age = now-when
             msg = '%s (%s): %s (%s)' % (
-                room['name'], room_latest['url'], when, age)
-            chat.send_message(msg)
+                room.name, room_latest['url'], when, age)
+            self.send_chat_message(homeroom, msg)
             logging.info(msg)
-            room['latest'] = room_latest
             if age > maxage:
-                self.notice(server, room['id'])
-                chat.send_message(
+                self.notice(room)
+                self.send_chat_message(
+                    homeroom,
                     '%s: Age threshold exceeded; sending a thawing notice' % (
-                        room['name']))
-                sleep(3)
-            sleep(3)
+                        room.name))
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+    Sloshy("sloshy.yaml", local=True).scan_rooms()
+
+
+if __name__ == '__main__':
+    main()
