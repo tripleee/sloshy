ef131b2772ba701272e65aaf9f0198c14d5127ed
tripleee <tripleee@users.noreply.github.com>
2022-11-14 22:06:15 +0200

    .github/workflows/test-pushed.yml: test room availability, too
    make-test-yaml.py: prerequisite for the previous

---
+++ make-test-yaml.py
@@ -0,0 +1,13 @@
+import yaml
+
+with open("sloshy.yaml", "r") as src:
+    config = yaml.safe_load(src)
+
+for room in config["servers"]["chat.stackoverflow.com"]["rooms"]:
+    if "role" in room and room["role"] == "home":
+        del room["role"]
+    if room["id"] == 233626:
+        room["role"] = "home"
+
+with open("room-test.yaml", "w") as dest:
+    yaml.safe_dump(config, dest)
