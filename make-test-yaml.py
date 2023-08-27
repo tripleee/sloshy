54941a655afafe2924486511599c3acbb9ced62a
tripleee <tripleee@users.noreply.github.com>
2023-08-27 13:39:14 +0300

    make-test-yaml.py: also strip any "cc" room

--- make-test-yaml.py
+++ make-test-yaml.py
@@ -4,7 +4,7 @@ with open("sloshy.yaml", "r") as src:
     config = yaml.safe_load(src)
 
 for room in config["servers"]["chat.stackoverflow.com"]["rooms"]:
-    if "role" in room and room["role"] == "home":
+    if "role" in room and room["role"] in ("home", "cc"):
         del room["role"]
     if room["id"] == 233626:
         room["role"] = "home"
