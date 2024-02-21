2fecae90ed3afce27bdb9630664e72974547fe3d
tripleee <tripleee@users.noreply.github.com>
2024-02-21 10:03:20 +0200

    run.py: optional argument --circle-ci enables --verbose and changes greeting

--- run.py
+++ run.py
@@ -17,6 +17,15 @@ if __name__ == '__main__':
 
     assert 'SLOSHY_EMAIL' in os.environ
     assert 'SLOSHY_PASSWORD' in os.environ
-    sloshy = Sloshy("sloshy.yaml")
+    if len(sys.argv) == 1:
+        greeting = "nightly run"
+        location = "Github Actions"
+        verbose = False
+    elif sys.argv[1] == "--circle-ci":
+        greeting = "circleci run"
+        location = "CircleCI"
+        verbose = True
+    sloshy = Sloshy("sloshy.yaml", verbose=verbose)
+    sloshy.config['nodename'] = f"{sloshy.nodename()} ({location})"
     sloshy.test_rooms()
-    sloshy.perform_scan("nightly run" if len(sys.argv) == 1 else sys.argv[1])
+    sloshy.perform_scan(greeting)
